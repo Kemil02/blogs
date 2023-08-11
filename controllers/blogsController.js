55f7677948aacb5dc068a59dec3e65ff3ff7c557
Karl Emil Lemström <karl.lemstrom@aalto.fi>
2023-08-11 17:24:35 +0300

    --> 4.13 finished

--- controllers/blogsController.js
+++ controllers/blogsController.js
@@ -1,26 +1,54 @@
 const blogRouter = require('express').Router()
 const morgan = require('morgan')
 const Blog = require('../models/blogModel')
+const mongoose = require('mongoose')
 
 morgan.token('content', (r) => JSON.stringify(r.body))
 blogRouter.use(morgan(':method :url :status :res[content-length] - :response-time ms :content'))
 
 
-blogRouter.get('/api/blogs', (request, response) => {
+blogRouter.get('/api/blogs', async (request, response) => {
 
-    Blog.find({})
-        .then(blogs => {
-            response.json(blogs)
-        })
+    const blogs = await Blog.find({})
+
+    response.json(blogs)
+
 })
 
-blogRouter.post('/api/blogs', (request, response) => {
-    const blog = new Blog(request.body)
+blogRouter.post('/api/blogs', async (request, response) => {
 
-    blog.save()
-        .then(result => {
-            response.status(201).json(result)
-        })
+    if (!request.body.url || !request.body.title) {
+        response.status(400).json('Bad request')
+    }
+    else {
+        const blog = new Blog(request.body)
+
+        const res = await blog.save()
+
+        response.status(201).json(res)
+    }
+
 })
+
+blogRouter.delete('/api/blogs/:id', async (request, response) => {
+    const inputID = request.params.id
+
+    if (!mongoose.isValidObjectId(inputID)) {
+        response.status(404).end()
+    }
+    else {
+        const blogExists = await Blog.exists({ _id: inputID })
+
+        if (blogExists) {
+
+            await Blog.findByIdAndRemove(inputID)
+
+            response.status(204).end()
+        }
+        else {
+            response.status(404).end()
+        }
+    }
+}) 
 
 module.exports = blogRouter
--- models/blogModel.js
+++ models/blogModel.js
@@ -1,10 +1,22 @@
 const mongoose = require('mongoose')
 
-const Blog = new mongoose.Schema({
-    title: String,
-    author: String,
-    url: String,
-    likes: Number
-})
+const BlogSchema =
+    new mongoose.Schema({
+        title: String,
+        author: String,
+        url: String,
+        likes: Number
+    }, {
+        toJSON: {
+            transform: (document, returnedObject) => {
+                returnedObject.id = returnedObject._id.toString()
+                delete returnedObject._id
+                delete returnedObject.__v
+                if (!returnedObject.likes) {
+                    returnedObject.likes = 0
+                }
+            }
+        }
+    })
 
-module.exports = mongoose.model('Blog', Blog)
+module.exports = mongoose.model('Blog', BlogSchema)
